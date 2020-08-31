cd5e5e5e75ed2d89eb1eb1f81350974c154be6a5
Bioruebe <william@nature-space.de>
2020-08-31 14:36:44 +0200

    Updated for BioLib 2.0.0

--- cicdec/Program.cs
+++ cicdec/Program.cs
@@ -7,21 +7,27 @@ using System.Text;
 using ICSharpCode.SharpZipLib;
 using ICSharpCode.SharpZipLib.BZip2;
 using BioLib;
+using BioLib.Streams;
 
 namespace cicdec {
 	class Program {
+		private const string VERSION = "2.1.1";
+		private const string PROMPT_ID = "cicdec_overwrite";
+
 		private const int SEARCH_BUFFER_SIZE = 1024 * 1024;
 		private const int BLOCK_HEADER_SIZE = 16 + 16 + 32;
 		private static readonly byte[] DATA_SECTION_SIGNATURE = {0x77, 0x77, 0x67, 0x54, 0x29, 0x48};
+
+		private static string inputFile;
 		private static string outputDirectory;
 		private static bool dumpBlocks;
 		private static bool simulate;
 		private static int installerVersion = -1;
+
+		private static MemoryStream fileListStream;
 		private static List<FileInfo> filesInfos;
 		private static long dataBlockStartPosition = -1;
 		private static int failedExtractions = 0;
-		private static string inputFile;
-		private static MemoryStream fileListStream;
 
 		static void Main(string[] args) {
 			const string USAGE = "[<options>...] <installer> [<output_directory>]\n\n" + 
@@ -29,7 +35,7 @@ namespace cicdec {
 								 "  -v <version>\tExtract as installer version <version>. Auto-detection might not always work correctly, so it is possible to explicitly set the installer version.\n\n" + 
 								 "  -db\tDump blocks. Save additional installer data like registry changes, license files and the uninstaller. This is considered raw data and might not be readable or usable.\n\n" + 
 								 "  -si\tSimulate extraction without writing files to disk.";
-			Bio.Header("cicdec - A Clickteam Install Creator unpacker", "2.1.0", "2019-2020", "Extracts files from installers made with Clickteam Install Creator", USAGE);
+			Bio.Header("cicdec - A Clickteam Install Creator unpacker", VERSION, "2019-2020", "Extracts files from installers made with Clickteam Install Creator", USAGE);
 			
 			inputFile = ParseCommandLine(args);
 			var inputStream = File.OpenRead(inputFile);
@@ -43,13 +49,13 @@ namespace cicdec {
 
 			Bio.Cout("Starting extraction at offset " + startOffset + "\n");
 
-			inputStream.Seek(startOffset, SeekOrigin.Begin);
+			inputStream.Position = startOffset;
 
 			// The data section consists of a varying number of data blocks,
 			// whose headers give information about the type of data contained inside.
 			while (inputStream.Position + BLOCK_HEADER_SIZE <= inputStreamLength) {
 				var blockId = binaryReader.ReadUInt16();
-				inputStream.Seek(2, SeekOrigin.Current); // unknown
+				inputStream.Skip(2); // unknown
 				var blockSize = binaryReader.ReadUInt32();
 				var blockType = (BLOCK_TYPE) blockId;
 				var nextBlockPos = inputStream.Position + blockSize;
@@ -59,13 +65,13 @@ namespace cicdec {
 				if (blockType == BLOCK_TYPE.FILE_DATA) {
 					// Data block should always be last, but better be safe and parse all other blocks before
 					dataBlockStartPosition = inputStream.Position;
+					
 					if (dumpBlocks) {
-						using (var ms = new MemoryStream()) {
-							Bio.CopyStream(inputStream, ms, (int) blockSize, false);
+						using (var ms = inputStream.Extract((int)blockSize)) {
 							SaveToFile(ms, outputFileName);
 						}
 					}
-					inputStream.Seek(blockSize, SeekOrigin.Current);
+
 					continue;
 				}
 				else if (blockType == BLOCK_TYPE.FILE_LIST) {
@@ -74,17 +80,16 @@ namespace cicdec {
 
 					if (dumpBlocks) SaveToFile(fileListStream, outputFileName);
 
-					fileListStream.Seek(0, SeekOrigin.Begin);
+					fileListStream.MoveToStart();
 				}
 				else if (dumpBlocks) {
-					Bio.Debug("Dumping block");
 					using (var decompressedStream = UnpackStream(binaryReader, blockSize)) {
 						SaveToFile(decompressedStream, outputFileName);
 					}
 				}
 
 				Bio.Debug("Pos: " + inputStream.Position + ", expected: " + nextBlockPos);
-				inputStream.Seek(nextBlockPos, SeekOrigin.Begin);
+				inputStream.Position = nextBlockPos;
 			}
 
 			if (fileListStream == null) Bio.Error("File list could not be read. Please send a bug report if you want the file to be supported in a future version.", Bio.EXITCODE.NOT_SUPPORTED);
@@ -183,10 +188,6 @@ namespace cicdec {
 			long dataStreamLength) {
 			if (installerVersion > -1) return installerVersion;
 
-			//var version = FileVersionInfo.GetVersionInfo(path).ProductVersion;
-			//installerVersion = Convert.ToInt32(version.Substring(version.Length - 2));
-			//Bio.Debug("Guessed installer version " + installerVersion);
-
 			if (TestInstallerVersion("40 - Free", TryParse40, decompressedStream, binaryReader, fileNumber, dataStreamLength)) return 40;
 			if (TestInstallerVersion("30 - Pro", TryParse30, decompressedStream, binaryReader, fileNumber, dataStreamLength)) return 30;
 			if (TestInstallerVersion("20 - Legacy", TryParse20, decompressedStream, binaryReader, fileNumber, dataStreamLength)) return 20;
@@ -206,20 +207,22 @@ namespace cicdec {
 					Bio.Debug(string.Format("Node {0} at offset {1}, size: {2}, end: {3}", i, fileInfo.nodeStart, fileInfo.nodeSize, fileInfo.nodeEnd));
 
 					if (!fileInfo.IsValid(dataStreamLength)) {
-						decompressedStream.Seek(pos, SeekOrigin.Begin);
+						Bio.Debug(fileInfo);
+						Bio.Debug("Invalid file info");
+						decompressedStream.Position = pos;
 						return false;
 					}
 
-					if (fileInfo.type != 0) decompressedStream.Seek(fileInfo.nodeEnd, SeekOrigin.Begin);
+					if (fileInfo.type != 0) decompressedStream.Position = fileInfo.nodeEnd;
 				}
 				catch (EndOfStreamException) {
-					Bio.Debug("Exception occured");
-					decompressedStream.Seek(pos, SeekOrigin.Begin);
+					Bio.Debug("End of Stream reached while parsing file list");
+					decompressedStream.Position = pos;
 					return false;
 				}
 			}
 
-			decompressedStream.Seek(pos, SeekOrigin.Begin);
+			decompressedStream.Position = pos;
 			return true;
 		}
 
@@ -254,28 +257,28 @@ namespace cicdec {
 
 			switch ((COMPRESSION) compressionMethod) {
 				case COMPRESSION.NONE:
-					Bio.CopyStream(binaryReader.BaseStream, decompressedStream, (int) blockSize, false);
+					binaryReader.BaseStream.Copy(decompressedStream, (int) blockSize);
 					break;
 				case COMPRESSION.DEFLATE:
-					binaryReader.BaseStream.Seek(2, SeekOrigin.Current);
+					binaryReader.BaseStream.Skip(2);
 					using (var deflateStream = new DeflateStream(binaryReader.BaseStream, CompressionMode.Decompress, true)) {
-						Bio.CopyStream(deflateStream, decompressedStream, (int) decompressedSize, false);
+						deflateStream.Copy(decompressedStream, (int) decompressedSize);
 					}
 					break;
 				case COMPRESSION.BZ2:
 					using (var bzip2Stream = new BZip2InputStream(binaryReader.BaseStream)) {
 						bzip2Stream.IsStreamOwner = false;
-						Bio.CopyStream(bzip2Stream, decompressedStream, (int) decompressedSize, false);
+						bzip2Stream.Copy(decompressedStream, (int) decompressedSize);
 					}
 					break;
 				default:
 					Bio.Warn("Unknown compression method, data might be encrypted and cannot be unpacked. Skipping block.");
 					decompressedStream.Dispose();
-					binaryReader.BaseStream.Seek(blockSize, SeekOrigin.Current);
+					binaryReader.BaseStream.Skip(blockSize);
 					return null;
 			}
 
-			//binaryReader.BaseStream.Seek(blockSize, SeekOrigin.Current);
+			//binaryReader.BaseStream.Skip(blockSize);
 			return decompressedStream;
 		}
 
@@ -283,32 +286,16 @@ namespace cicdec {
 			if (simulate) return true;
 
 			if (stream == null) {
-				Bio.Debug("Failed to save stream to file. Stream is null");
+				Bio.Warn("Failed to save stream to file. Stream is null");
 				return false;
 			}
 
+			stream.MoveToStart();
 			var filePath = Path.Combine(outputDirectory, fileName);
-
 			//Bio.Cout("Saving decompressed block to " + outputFilePath);
 
-			// Create subfolders
 			try {
-				var fileDirectory = Path.GetDirectoryName(filePath);
-				if (fileDirectory != null) Directory.CreateDirectory(fileDirectory);
-			}
-			catch (Exception e) {
-				Bio.Error("Failed to create output directory. Please ensure you have sufficient permissions or try another output directory.\n" + e, Bio.EXITCODE.IO_ERROR);
-			}
-
-			filePath = Bio.EnsureFileDoesNotExist(filePath, "cicdec_overwrite");
-			if (filePath == null) return true;
-
-			// Save
-			try {
-				using (var fileStream = File.Create(filePath)) {
-					stream.Seek(0, SeekOrigin.Begin);
-					stream.CopyTo(fileStream);
-				}
+				if (!stream.WriteToFile(filePath, PROMPT_ID)) return true;
 
 				if (fileInfo != null) {
 					File.SetCreationTime(filePath, fileInfo.created);
@@ -328,7 +315,7 @@ namespace cicdec {
 			var binaryReader  = new BinaryReader(decompressedStream);
 			var fileNumber = binaryReader.ReadUInt16();
 			filesInfos = new List<FileInfo>();
-			decompressedStream.Seek(2, SeekOrigin.Current); // Unknown. Maybe fileNumber is 4 bytes?
+			decompressedStream.Skip(2); // Unknown. Maybe fileNumber is 4 bytes?
 			Bio.Cout("\n" + fileNumber + " files in installer\n");
 
 			installerVersion = GetInstallerVersion(decompressedStream, binaryReader, fileNumber, dataStreamLength);
@@ -356,16 +343,16 @@ namespace cicdec {
 #if DEBUG
 				if (dumpBlocks) {
 					using (var ms = new MemoryStream((int) fileInfo.nodeSize)) {
-						decompressedStream.Seek(fileInfo.nodeStart, SeekOrigin.Begin);
-						Bio.CopyStream(decompressedStream, ms, (int) fileInfo.nodeSize, true);
-						decompressedStream.Seek(fileInfo.nodeEnd, SeekOrigin.Begin);
+						decompressedStream.Position = fileInfo.nodeStart;
+						decompressedStream.Copy(ms, (int) fileInfo.nodeSize);
+						decompressedStream.Position = fileInfo.nodeEnd;
 						SaveToFile(ms, "FileMeta" + i + ".bin");
 					}
 				}
 #endif
 
 				if (fileInfo.type != 0) {
-					decompressedStream.Seek(fileInfo.nodeEnd, SeekOrigin.Begin);
+					decompressedStream.Position = fileInfo.nodeEnd;
 					continue;
 				}
 
@@ -378,9 +365,9 @@ namespace cicdec {
 			var fileInfo = new FileInfo(decompressedStream.Position, binaryReader.ReadUInt16(), binaryReader.ReadUInt16());
 			if (fileInfo.type != 0) return fileInfo;
 
-			decompressedStream.Seek(2, SeekOrigin.Current);
+			decompressedStream.Skip(2);
 			fileInfo.SetFileInfos(binaryReader.ReadUInt32(), binaryReader.ReadUInt32(), binaryReader.ReadUInt32(), binaryReader.ReadUInt32());
-			decompressedStream.Seek(16, SeekOrigin.Current);
+			decompressedStream.Skip(16);
 			fileInfo.SetFileTimes(binaryReader.ReadInt64(), binaryReader.ReadInt64(), binaryReader.ReadInt64());
 
 			ReadFilePath(decompressedStream, binaryReader, fileInfo);
@@ -392,9 +379,9 @@ namespace cicdec {
 			var fileInfo = new FileInfo(decompressedStream.Position, binaryReader.ReadUInt16(), binaryReader.ReadUInt16());
 			if (fileInfo.type != 0) return fileInfo;
 
-			decompressedStream.Seek(2, SeekOrigin.Current);
+			decompressedStream.Skip(2);
 			fileInfo.SetFileInfos(binaryReader.ReadUInt32(), binaryReader.ReadUInt32(), binaryReader.ReadUInt32(), binaryReader.ReadUInt32());
-			decompressedStream.Seek(18, SeekOrigin.Current);
+			decompressedStream.Skip(18);
 			fileInfo.index = binaryReader.ReadUInt32();
 			fileInfo.SetFileTimes(binaryReader.ReadInt64(), binaryReader.ReadInt64(), binaryReader.ReadInt64());
 
@@ -407,18 +394,18 @@ namespace cicdec {
 			var fileInfo = new FileInfo(decompressedStream.Position, binaryReader.ReadUInt32(), binaryReader.ReadUInt16());
 			if (fileInfo.type != 0) return fileInfo;
 
-			decompressedStream.Seek(3, SeekOrigin.Current);
+			decompressedStream.Skip(3);
 			// Empty dummy files are missing file time attributes,
 			// so the node is shorter than usual
 			if (binaryReader.ReadByte() == 0xE2) {
-				decompressedStream.Seek(30, SeekOrigin.Current);
+				decompressedStream.Skip(30);
 			}
 			else {
-				decompressedStream.Seek(14, SeekOrigin.Current);
+				decompressedStream.Skip(14);
 				var uncompressedSize = binaryReader.ReadUInt32();
 				var offset = binaryReader.ReadUInt32();
 				var compressedSize = binaryReader.ReadUInt32();
-				decompressedStream.Seek(4, SeekOrigin.Current);
+				decompressedStream.Skip(4);
 				fileInfo.SetFileInfos(offset, compressedSize, 0, uncompressedSize);
 				fileInfo.SetFileTimes(binaryReader.ReadInt64(), binaryReader.ReadInt64(), binaryReader.ReadInt64());
 			}
@@ -437,7 +424,7 @@ namespace cicdec {
 			var pathBuffer = new byte[pathLength];
 			binaryReader.Read(pathBuffer, 0, (int) pathLength);
 
-			// If the installer should create a shortcut for a file, it's name is 
+			// If the installer should create a shortcut for a file, its name is 
 			// placed right after the output path. To make sure we don't have parts
 			// of the .lnk file name in our buffer, we search for a 0 byte seperator
 			// and split the array (ignoring the shortcut).
@@ -457,12 +444,12 @@ namespace cicdec {
 		}
 
 		static void ExtractFiles(Stream inputStream, BinaryReader binaryReader) {
-			inputStream.Seek(dataBlockStartPosition + 4, SeekOrigin.Begin);
+			inputStream.Position = dataBlockStartPosition + 4;
 			
 			for (var i = 0; i < filesInfos.Count; i++) {
 				var fileInfo = filesInfos[i];
 				Bio.Debug(fileInfo);
-				inputStream.Seek(dataBlockStartPosition + fileInfo.offset + 4, SeekOrigin.Begin);
+				inputStream.Position = dataBlockStartPosition + fileInfo.offset + 4;
 				Bio.Cout(string.Format("{0}/{1}\t{2}", i + 1, filesInfos.Count, fileInfo.path));
 
 				// Empty files
